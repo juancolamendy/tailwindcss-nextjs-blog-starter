48e742be911b56f77520be4ff35d432b83cb7823
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-02-09 09:47:12 -0500

    initial set up

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './layouts/**/*.{js,jsx}'],
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}', './layouts/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
