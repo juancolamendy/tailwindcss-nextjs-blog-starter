7a310ece55c3f9e31508df98a4ac1b3da1ddd282
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-02-11 17:05:31 -0500

    Implement blogs frontpage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import { getAllFilesFrontMatter } from '../lib/mdx';
 
 import { PageSEO } from '../components/SEO';
+import { Link } from '../components/Link';
+import { Tag } from '../components/Tag';
+
+import constants from '../lib/utils/constants';
+import siteMetadata from '../lib/utils/constants/siteMetadata';
+import { formatDate } from '../lib/utils/dateutils';
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blogs');
@@ -14,7 +20,67 @@ const Index = ({ posts }) => {
   return (
     <>
       <PageSEO title="Blog Starter by JC Olamendy" description="Blog Starter will help you build your blog platform faster" />
-      Hello World!
+      <div className="border-b-2 border-gray-500">
+        <div className="space-y-2 py-2 md:space-y-5 md:py-4">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-600 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7">
+            {siteMetadata.meta.blogsDescription}
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, constants.MaxDisplay).map(frontMatter => {
+            const { slug, date, title, summary, tags } = frontMatter
+            return (
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   );
 }
