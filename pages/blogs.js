d843754d6400f18f2fe7e022c5cb9e9d07f9b4e7
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-03-03 11:17:37 -0500

    add breadcrum to blogs

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,9 +1,21 @@
 import { PageSEO } from '../components/SEO';
 
 import { PostsPagedListView } from '../components/PostsList';
+import { Breadcrumb } from '../components/Post';
 
 import { getAllFilesFrontMatter } from '../lib/mdx';
 import siteMetadata from '../data/siteMetadata';
+
+const breadcrum = [
+  {
+    href: '/',
+    text: 'Home'
+  },
+  {
+    href: '/blogs',
+    text: 'Posts'
+  }
+];
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blogs');
@@ -14,6 +26,7 @@ const Posts = ({ posts, currentPage }) => {
   return (
   <>
     <PageSEO title={siteMetadata.meta.title} description={siteMetadata.meta.description} />
+    <Breadcrumb list={breadcrum} />
     <main className="mb-auto">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
         <PostsPagedListView 
