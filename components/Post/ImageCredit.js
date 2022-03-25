f9057971cd62530e975ef265e7f33480e49b1825
Juan Carlos Olamendy <juan.olamendy@bluvision.com>
2022-03-25 11:26:35 -0400

    fix image credit

--- components/Post/ImageCredit.js
+++ components/Post/ImageCredit.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const ImageCredit = ({personRef, person, imageRef, imageSource}) => {
   return (
-  <p>
-    Photo 
-    { person && (<span>by <a target="_blank" rel="noopener noreferrer" href="{personRef}" className="underline link-text">{person}</a></span>)}
-    on 
-    <a target="_blank" rel="noopener noreferrer" href="{imageRef}" className="underline link-text">{imageSource}</a>
+  <p className="flex flex-row space-x-1">
+    <span>Photo</span>
+    { person && personRef && (<span>by <a target="_blank" rel="noopener noreferrer" href={personRef} className="underline link-text">{person}</a></span>)}
+    <span>on</span>
+    { imageRef && imageSource && <a target="_blank" rel="noopener noreferrer" href={imageRef} className="underline link-text">{imageSource}</a> }
   </p>
   );
 };
